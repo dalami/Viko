06d147c12e6dcedf2bd5f3b2df66977bb5231d04
Dalami <diegoalami@gmail.com>
2026-05-19 16:00:54 -0300

    headers

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.mercadopago.com https://*.mercadolibre.com https://*.mlstatic.com",
+              "style-src 'self' 'unsafe-inline' https://*.mercadopago.com https://*.mlstatic.com",
+              "img-src 'self' data: blob: https://*.mercadopago.com https://*.mlstatic.com https://*.supabase.co https://images.unsplash.com",
+              "frame-src 'self' https://*.mercadopago.com https://*.mercadolibre.com",
+              "connect-src 'self' https://*.mercadopago.com https://*.mercadolibre.com https://*.supabase.co",
+              "font-src 'self' data: https://*.mlstatic.com",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
