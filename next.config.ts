912245c54331185a1b42be4afbaa7e0d7bf2d0f7
Dalami <diegoalami@gmail.com>
2026-05-19 16:06:23 -0300

    fix headers

--- next.config.ts
+++ next.config.ts
@@ -10,27 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.mercadopago.com https://*.mercadolibre.com https://*.mlstatic.com https://www.googletagmanager.com https://www.google-analytics.com",
-              "style-src 'self' 'unsafe-inline' https://*.mercadopago.com https://*.mlstatic.com https://fonts.googleapis.com",
-              "img-src 'self' data: blob: https://*.mercadopago.com https://*.mlstatic.com https://*.supabase.co https://images.unsplash.com https://www.googletagmanager.com",
-              "frame-src 'self' https://*.mercadopago.com https://*.mercadolibre.com",
-              "connect-src 'self' https://*.mercadopago.com https://*.mercadolibre.com https://*.supabase.co https://www.google-analytics.com https://region1.google-analytics.com",
-              "font-src 'self' data: https://*.mlstatic.com https://fonts.gstatic.com",
-            ].join('; '),
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
